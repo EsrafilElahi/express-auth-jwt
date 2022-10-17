9898cc264fdcfc851051604af498a438ff5b541e
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-10-17 22:16:05 +0330

    add text

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+  console.log(`server is running on port ${PORT}`);
   connectDB();
 });
